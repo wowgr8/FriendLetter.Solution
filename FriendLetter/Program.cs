a169a77e31ce25e6eba620c0ed772cb801e93a1a
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-14 16:19:07 -0800

    add a Main() entry point method

--- FriendLetter/Program.cs
+++ FriendLetter/Program.cs
@@ -5,6 +5,16 @@ namespace FriendLetter
 {
   public class Program
   {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
+      host.Run();
+    }
   }
 }
