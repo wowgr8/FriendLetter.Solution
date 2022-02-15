e259ba72798f9d7ee140588b5882de1a88c24a57
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-14 16:10:00 -0800

    add namespace and class declaration

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -3,3 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+  public class Startup
+  {
+
+  }
+}
